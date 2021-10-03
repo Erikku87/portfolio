e66acd7f261a92d9cf16c826e4bf2d0781002f63
Erikku87 <erikkudesign@gmail.com>
2021-10-03 19:47:28 +0200

    fixing profile

--- components/Profile.js
+++ components/Profile.js
@@ -3,11 +3,9 @@ import React, { useState } from "react";
 import Image from "next/dist/client/image";
 import Link from "next/dist/client/link";
 
-
 const Profile = () => {
   const navs = ["Identity", "Who am I?", "Projects", "Contact"];
   const [navSelected, setNavSelected] = useState("");
-  
 
   return (
     <div className={stylesProfile.profileframe}>
@@ -38,7 +36,6 @@ const Profile = () => {
         </div>
         <div id="profileinfo" className={stylesProfile.profileinfo}>
           {currentNav(navSelected)}
-
         </div>
       </div>
     </div>
@@ -56,15 +53,10 @@ if (typeof window === "object") {
 }
 
 const currentNav = (thisNav) => {
-    
-    
-
-    if(thisNav === "Identity"){
+  if (thisNav === "Identity") {
     return (
       <>
-      <div className={stylesProfile.navHeading}>
-      {thisNav}
-    </div> 
+        <div className={stylesProfile.navHeading}>{thisNav}</div>
         <div>
           <ul>
             <li className={stylesProfile.label}>Naam:</li>
@@ -79,43 +71,60 @@ const currentNav = (thisNav) => {
         </div>
       </>
     );
-}
+  }
 
-    if(thisNav === "Who am I?"){
-        return (
-        <>
-        <div className={stylesProfile.navHeading}>
-        {thisNav}
-        </div> 
-            <div className={stylesProfile.whoami}>
-            <p>
-            Phasellus at bibendum velit, mattis fermentum nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pellentesque diam orci, ut aliquam ipsum finibus nec. Curabitur vehicula nunc eget magna aliquet, laoreet placerat nunc condimentum. Vivamus ut ullamcorper lectus, nec finibus orci. Sed viverra tincidunt purus at porttitor. Duis rhoncus et nulla in dictum. Praesent placerat rutrum metus, nec consectetur tortor fringilla non. Cras non vulputate magna. Morbi placerat magna augue.
-            </p>
-            </div>
-        </>
-        );
-    }
+  if (thisNav === "Who am I?") {
+    return (
+      <>
+        <div className={stylesProfile.navHeading}>{thisNav}</div>
+        <div className={stylesProfile.whoami}>
+          <p>
+            Phasellus at bibendum velit, mattis fermentum nunc. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Etiam pellentesque diam
+            orci, ut aliquam ipsum finibus nec. Curabitur vehicula nunc eget
+            magna aliquet, laoreet placerat nunc condimentum. Vivamus ut
+            ullamcorper lectus, nec finibus orci. Sed viverra tincidunt purus at
+            porttitor. Duis rhoncus et nulla in dictum. Praesent placerat rutrum
+            metus, nec consectetur tortor fringilla non. Cras non vulputate
+            magna. Morbi placerat magna augue.
+          </p>
+        </div>
+      </>
+    );
+  }
 
-    if(thisNav === "Projects"){
-        //router.push('/Erikku.design#portfolioHeading', undefined, { shallow: true })
-        //document.getElementById('portfolioHeading').scrollIntoView();
-        window.scrollTo({top: '700', behavior: 'smooth'});
-        
-    }
-    if(thisNav === "Contact"){
-        return (
-        <>
-        <div className={stylesProfile.navHeading}>
-        {thisNav}
-        </div> 
-            <div className={stylesProfile.whoami}>
-            Hier komt een Forum
-            </div>
-        </>
-        );
-    }
-  };
+  if (thisNav === "Projects") {
+    //router.push('/Erikku.design#portfolioHeading', undefined, { shallow: true })
+    //document.getElementById('portfolioHeading').scrollIntoView();
+    window.scrollTo({ top: "700", behavior: "smooth" });
+  }
+  if (thisNav === "Contact") {
+    return (
+      <>
+        <div className={stylesProfile.navHeading}>{thisNav}</div>
+        <div className={stylesProfile.whoami}>Hier komt een Forum</div>
+      </>
+    );
+  }
 
+  return (
+    <>
+      <div className={stylesProfile.navHeading}>Identity</div>
+      <div>
+        <ul>
+          <li className={stylesProfile.label}>Naam:</li>
+          <li>Eric Tjhie</li>
+          <li className={stylesProfile.label}>Geboren op:</li>
+          <li>13 - 10 - 1987</li>
+          <li className={stylesProfile.label}>Telefoon:</li>
+          <li>0619049059</li>
+          <li className={stylesProfile.label}>E-mail:</li>
+          <li>Erikkudesign@gmail.com</li>
+        </ul>
+      </div>
+    </>
+  );
 
+};
 
 export default Profile;
